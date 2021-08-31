ea51f57974fb263b407d70cb4b084a52b3c455ac
Ahmad Mostafa Masud <jmasudd@gmail.com>
2021-08-31 15:42:40 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Homepage from "./Components/Homepage/Homepage";
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
