7212ed0d4df4f8705f735916230db1841494bb86
Ahmad Mostafa Masud <jmasudd@gmail.com>
2021-09-01 20:15:56 +0600

    added navigation functionality to the hats page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage.component";
 
 import React, { Component } from "react";
+
+const HatsPage = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
+const topic = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>Topic</h1>
+    </div>
+  );
+};
+
+const topicdetail = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h1>this is the topic no: {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Homepage />
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/topic" component={topic} />
+        <Route path="/topic/:topicId" component={topicdetail} />
       </div>
     );
   }
