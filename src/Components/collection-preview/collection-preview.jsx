7ad3c98e9e6c3ffd6661301a2cd5ef12d3cf7791
Ahmad Mostafa Masud <jmasudd@gmail.com>
2021-09-10 11:58:10 +0600

    added styling to the shop page

--- src/Components/collection-preview/collection-preview.jsx
+++ src/Components/collection-preview/collection-preview.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./collection-preview.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="items">
+      <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+          .map(({ id, ...otherItemprops }) => (
+            <CollectionItem key={id} {...otherItemprops} />
           ))}
       </div>
     </div>
