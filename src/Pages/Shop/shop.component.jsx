575888b3cadb072046610196744c9ab8212b4d8a
Ahmad Mostafa Masud <jmasudd@gmail.com>
2021-09-10 10:03:22 +0600

    sep 10 update

---
+++ src/Pages/Shop/shop.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import SHOP_DATA from "./shopdata";
+import CollectionPreview from "../../Components/collection-preview/collection-preview";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...othercollection }) => (
+          <CollectionPreview key={id} {...othercollection} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
