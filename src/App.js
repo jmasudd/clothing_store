1fc71bc2c5fd7c350a042e89783477892e043576
Ahmad Mostafa Masud <jmasudd@gmail.com>
2021-10-02 15:12:15 +0600

    added a sign up component in signin and signup page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage.component";
 import ShopPage from "./Pages/Shop/shop.component";
 import Header from "./Components/header/header.component";
+import SignInandsignUp from "./Pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component";
 
 import React, { Component } from "react";
 
@@ -24,6 +25,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInandsignUp} />
         </Switch>
       </div>
     );
---
+++ src/Pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sign-in-and-sign-up-page.style.scss";
+import SignIn from "../../Components/signin/signin";
+
+const SignInandsignUp = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInandsignUp;
