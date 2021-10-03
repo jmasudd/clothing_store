da2f45f0ffd31df0ccf944bdd6d1a02b9a261502
Ahmad Mostafa Masud <jmasudd@gmail.com>
2021-10-03 09:20:32 +0600

    added a custom button component and styked sign in component

--- src/Components/signin/signin.jsx
+++ src/Components/signin/signin.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./signin.scss";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -24,7 +25,7 @@ class SignIn extends React.Component {
 
   render() {
     return (
-      <div className="sigin">
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handlesubmit}>
@@ -44,7 +45,7 @@ class SignIn extends React.Component {
             handlechange={this.handlechange}
             required
           />
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );
